a91e56217b698f36256db56e0c1673ac1a193d5e
Theo <theoslater1@gmail.com>
2024-08-22 15:36:05 +0100

    Vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ThemeProvider from "./theme/ThemeProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <ThemeProvider>
+      <SpeedInsights />
+      <Analytics />
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
